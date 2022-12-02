a6aa12e5dd1c494601dbc0c92b4b77c11230db95
Andru <a01640839@tec.mx>
2022-12-02 11:48:32 -0600

    d3-svg-charts-typesc

--- src/features/Analisis.js
+++ src/features/Analisis.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, StyleSheet} from "react-native";
-import { ProgressChart } from "react-native-chart-kit";
 
 import { spacing, fontSizes } from "../utils/sizes";
 import { colors } from "../utils/colors";
@@ -8,33 +7,6 @@ import { colors } from "../utils/colors";
 import { Logo } from "../components/Logo";
 
 import { Progreso } from "../components/Progreso";
-
-const MyProgressChart = () => {
-  return (
-    <>
-      <Text style={styles.header}>Progress Chart</Text>
-      <ProgressChart
-        data={[0.4, 0.6, 0.8]}
-        width={Dimensions.get('window').width - 16}
-        height={220}
-        chartConfig={{
-          backgroundColor: '#1cc910',
-          backgroundGradientFrom: '#eff3ff',
-          backgroundGradientTo: '#efefef',
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-        }}
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
-    </>
-  );
-};
 
 export const Analisis = ({met}) => {
   return (
