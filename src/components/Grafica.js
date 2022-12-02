9032d7452534aabeaa59ea17f3de6cd265c7fddb
Andru <a01640839@tec.mx>
2022-12-02 12:12:43 -0600

    typescript-screens

--- src/components/Grafica.js
+++ src/components/Grafica.js
@@ -1,8 +1,18 @@
-import React from "react"
-import { AreaChart } from 'react-native-svg-charts'
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import { Dots, Line } from '../screens/AreaChartScreen/chartAdds';
 
 export const Grafica = () => {
     const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
-    return <AreaChart style={{ height: 200 }} data={data} svg={{ fill: '#ADD8E6' }} />
+    return <AreaChart
+    style={{ height: 200 }}
+    data={data}
+    gridMin={-100}
+    gridMax={120}
+    contentInset={{ top: 30, bottom: 30 }}
+    svg={{ fill: '#ADD8E6' }}>
+    <Grid />
+    <Line />
+    <Dots />
+</AreaChart>
 
 }
