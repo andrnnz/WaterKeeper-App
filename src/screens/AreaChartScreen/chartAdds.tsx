2e578c6da4aac63a8211ae5545e84ad6e4c08562
Andru <a01640839@tec.mx>
2022-12-02 12:27:17 -0600

    tsconfig

--- src/screens/AreaChartScreen/chartAdds.tsx
+++ src/screens/AreaChartScreen/chartAdds.tsx
@@ -1,4 +1,4 @@
-import { Circle, Defs, LinearGradient, Path, Stop } from 'react-native-svg'
+import { Circle, Defs, Path, Stop } from 'react-native-svg'
 import React from 'react'
 
 interface LineProps {
@@ -34,11 +34,3 @@ export const Dots = (props: Partial<DecoratorProps>) => {
   )
 }
 
-export const Gradient = () => (
-  <Defs>
-    <LinearGradient id={'gradient'} x1={'0%'} y1={'0%'} x2={'0%'} y2={'100%'}>
-      <Stop offset={'0%'} stopColor={'rgb(194, 65, 244)'} stopOpacity={0.8} />
-      <Stop offset={'100%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.2} />
-    </LinearGradient>
-  </Defs>
-)
