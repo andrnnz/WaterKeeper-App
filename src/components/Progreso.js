5707cbc50612141b454a05e79c9d8bfadc8f7ac6
Andru <a01640839@tec.mx>
2022-12-02 11:03:45 -0600

    archLectura

--- src/components/Progreso.js
+++ src/components/Progreso.js
@@ -1,67 +1,9 @@
-import React, {useState, useEffect} from "react"; 
-import { collection, addDoc, getDocs, doc, getDoc, setDoc} from "firebase/firestore"; 
-
-import xd from '../utils/variables';
+import { Lectura } from "./Lectura"
 
 export const Progreso = () => {
-    const {app,db} = xd;
+  return (
 
-  class User {
-    constructor (born, first, last ) {
-        this.born = born;
-        this.first = first;
-        this.last = last;
-    }
-    toString() {
-        return this.born + ', ' + this.first + ', ' + this.last;
-    }
-  }
+    <Lectura/>
 
-  const userConverter = {
-    toFirestore: (user) => {
-        return {
-            born: user.born,
-            first: user.first,
-            last: user.last
-            };
-    },
-    fromFirestore: (snapshot, options) => {
-        const data = snapshot.data(options);
-        return new User(data.born, data.first, data.last);
-    }
-};
-
-  useEffect(() => {
-    const test = async() =>{
-
-      const usersRef = collection(db, "users");
-
-      await setDoc(doc(usersRef, "SP"), {
-        born: 1989, first: "Sexo", last: "Perez"});
-      await setDoc(doc(usersRef, "AN"), {
-        born: 2002, first: "And", last: "Si"});
-
-      try {
-        console.log("entro")
-        const querySnapshot = await getDocs(collection(db, "users"));
-        querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`);
-        });
-        const ref = doc(db, "users", "SP").withConverter(userConverter);
-        const docSnap = await getDoc(ref);
-        if (docSnap.exists()) {
-          // Convert to City object
-          const city = docSnap.data();
-          // Use a City instance method
-          console.log(city.toString());
-        } else {
-          console.log("No such document!");
-        }
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
-    }
-
-    test()
-  },[])
+  );
 }
